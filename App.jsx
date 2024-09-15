fdfe7a420e94f08b84bd373510b7feebcf8b4c50
Ian Hadrien <ianhadrien@gmail.com>
2024-09-14 23:07:54 -0300

    feat: add background login

--- App.jsx
+++ App.jsx
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, ImageBackground, ScrollView, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView className="bg-primary-purple pt-2">
+      <View className="flex items-start border border-red-500">
+        <ImageBackground
+          source={require("./src/imgs/FF.png")}
+          style={{ width: 279.14, height: 312.68 }}
+        >
+        <Text className="bg-black text-white">Open up App.js to start working on your app!</Text>
+        </ ImageBackground>
+      </View>
       <Text className="bg-black">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          primary: {
+            purple: '#4C33CC',
+            purple2: '#D4CCFF',
+            white: '#F7F5FA',
+            yellow: '#FFC042',
+            gray: '#BEBCCC',
+            gray2: '#4C4766',
+          }
+      },
+    },
   },
   plugins: [],
 }
